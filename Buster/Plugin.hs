92404fe2a3ced05fa1869a036de79ee9e1965e16
Paul Collier <paul@paulcollier.ca>
2010-07-14 18:05:23 -0700

    Config lookups across processes

--- Buster/Plugin.hs
+++ Buster/Plugin.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
-module Buster.Plugin (
-       commandPlugin, io, pluginMain, processorPlugin, respondChat,
+module Buster.Plugin (Machine, Plugin,
+       commandPlugin, pluginMain, processorPlugin,
+       io, lookupConfig, respondChat,
        loadPlugin, setupMachine,
        ) where
 
@@ -21,17 +22,7 @@ import System.Posix
 
 -- FOR PLUGIN USE
 
-data PluginImpl = PluginImpl {
-    pluginCommands :: Map String (Target -> String -> Plugin ()),
-    pluginProcessor :: Maybe (ServerMsg -> Plugin ())
-}
-
 type Plugin = ReaderT PluginState IO
-data PluginState = PluginState {
-    pluginRead, pluginWrite :: Handle
-}
-
-data PluginResp = ClientMsg IRCMsg | EndResponse deriving (Read, Show)
 
 commandPlugin cmds = PluginImpl (Map.fromList cmds) Nothing
 processorPlugin p  = PluginImpl (Map.empty) (Just p)
@@ -40,25 +31,26 @@ pluginMain :: PluginImpl -> IO ()
 pluginMain (PluginImpl {..}) = do
     [r, w] <- mapM fdToHandle =<< (map (Fd . read) `fmap` getArgs)
     mapM_ dontBuffer [stdout, stderr, r, w]
-    send w api
-    let st = PluginState r w
-    forever (recv r >>= either invalidReq doReq) `runReaderT` st
+    send w $ API (Map.keysSet pluginCommands) (isJust pluginProcessor)
+    forever loop `runReaderT` PluginState r w
   where
-    api = API (Map.keysSet pluginCommands) (isJust pluginProcessor)
+    loop = recv' >>= either invalidReq doReq
 
     doReq (ReqProcess msg)    = check ($ msg) pluginProcessor
     doReq (ReqCommand t ch a) = check ($ (ch, a)) (uncurry `fmap`
                                       Map.lookup t pluginCommands)
-    check f = maybe (io $ putStrLn "Request unsupported") $ \req -> do
-      f req
-      w <- asks pluginWrite
-      send w EndResponse
-
-    invalidReq = io . putStrLn . ("Invalid req: " ++)
+    check f = maybe (io $ putStrLn "Request unsupported")
+                    ((>> send' EndResponse) . f)
 
 respondChat :: Target -> String -> Plugin ()
-respondChat t s = do w <- asks pluginWrite
-                     send w $ ClientMsg $ PrivMsg t (Chat s)
+respondChat t s = send' $ ClientMsg $ PrivMsg t (Chat s)
+
+lookupConfig :: String -> Plugin (Maybe String)
+lookupConfig s = do send' $ ConfigLookup s
+                    recv' >>= either ((>> return Nothing) . invalidReq) gotIt
+  where
+    gotIt (ReqConfig k (Just v)) | k == s = return (Just v)
+    gotIt _                               = return Nothing
 
 io = liftIO :: IO a -> Plugin a
 
@@ -92,7 +84,7 @@ loadPlugin mach@(Machine {..}) nm
     addAPI a = do putStrLn "done."
                   return $ mach { machPlugins = Map.insert nm a machPlugins }
 
--- FOR INTERNAL USE
+-- MACHINE-SIDE INTERNALS
 
 data PluginIPC = PluginIPC {
     ipcPID :: ProcessID,
@@ -106,6 +98,7 @@ data API = API {
 } deriving (Read, Show)
 
 data MachineReq = ReqProcess ServerMsg | ReqCommand String Target String
+                  | ReqConfig String (Maybe String)
                   deriving (Read, Show)
 
 dispatchPlugins :: IORef Machine -> MessageProcessor
@@ -116,15 +109,15 @@ dispatchPlugins mRef msg = do
       PrivMsg t (Chat s@('!':_)) -> eval machPlugins t s
       _                          -> return ()
   where
-    doProc (PluginIPC {..}) = do send ipcWrite (ReqProcess msg)
-                                 recv ipcRead >>= doResponse'
+    doProc p = do send (ipcWrite p) (ReqProcess msg)
+                  handleResponses p
 
 eval ps t ('!':s) = case words s of
     (w:_) -> do case Map.fold (collectPlugins $ map toLower w) [] ps of
                   []  -> privMsg t ("No such command " ++ w ++ ".")
                   [p] -> do let arg = stripLeft $ drop (length w) s
                             send (ipcWrite p) (ReqCommand w t arg)
-                            recv (ipcRead p) >>= doResponse'
+                            handleResponses p
                   ps  -> privMsg t ("Ambiguous command " ++ w ++ ".") -- TODO
     []    -> privMsg t "Command expected."
   where
@@ -140,18 +133,49 @@ recv h = do l <- liftIO $ hGetLine h
             return $ case reads l of [(a, "")] -> Right a
                                      _         -> Left l
 
-doResponse (ClientMsg msg) = case msg of
+handleResponses :: PluginIPC -> Net ()
+handleResponses (PluginIPC {..}) = handleOne
+  where
+    handleOne = recv ipcRead >>= either invalidResp go
+
+    go EndResponse = return ()
+    go resp        = doResponse ipcWrite resp >> handleOne
+
+doResponse _ (ClientMsg msg) = case msg of
     PrivMsg t msg -> privMsg t $ formatChat msg
     _             -> liftIO $ putStrLn $ "TODO: Perform " ++ show msg
   where
     formatChat (Chat s)   = s
     formatChat (Action s) = "\001ACTION " ++ s ++ "\001"
 
-doResponse' :: Either String (Maybe PluginResp) -> Net ()
-doResponse' = either (liftIO . putStrLn . ("Got invalid response: " ++))
-                     (maybe (return ()) doResponse)
+doResponse w (ConfigLookup k) = do v <- getConfig k
+                                   liftIO $ send w $ ReqConfig k v
 
 privMsg :: Target -> String -> Net ()
 privMsg t s = write "PRIVMSG" [pretty t "", s]
+
+invalidResp = liftIO . putStrLn . ("Invalid resp: " ++)
+
+-- PLUGIN-SIDE INTERNALS
+
+data PluginImpl = PluginImpl {
+    pluginCommands :: Map String (Target -> String -> Plugin ()),
+    pluginProcessor :: Maybe (ServerMsg -> Plugin ())
+}
+
+data PluginState = PluginState {
+    pluginRead, pluginWrite :: Handle
+}
+
+data PluginResp = ClientMsg IRCMsg | ConfigLookup String | EndResponse
+                  deriving (Read, Show)
+
+send' :: PluginResp -> Plugin ()
+send' = (asks pluginWrite >>=) . flip send
+
+recv' :: Plugin (Either String MachineReq)
+recv' = asks pluginRead >>= recv
+
+invalidReq = io . putStrLn . ("Invalid req: " ++)
 
  -- vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
--- plugins/Identity.hs
+++ plugins/Identity.hs
@@ -7,7 +7,7 @@ main = pluginMain $ processorPlugin noticer
 
 noticer (Name "NickServ" _ _, Notice (Nick _) m)
   | "/msg NickServ IDENTIFY" `isInfixOf` m = do
-    let pass = Nothing -- TODO: pass <- getConfig "NickServPassword"
+    pass <- lookupConfig "NickServPassword"
     case pass of Just p  -> respondChat (Nick "NickServ") ("identify " ++ p)
                  Nothing -> io $ putStrLn "Need 'NickServPassword' config!"
   | "Password accepted" `isInfixOf` m = io $ putStrLn "Identified self."
