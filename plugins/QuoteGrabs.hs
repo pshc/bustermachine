5a7289f6719e3203f79c9fe9d1d654deee110c31
Paul Collier <paching@gmail.com>
2010-08-06 14:50:53 -0700

    !!rq

--- plugins/QuoteGrabs.hs
+++ plugins/QuoteGrabs.hs
@@ -1,21 +1,32 @@
-
+{-# LANGUAGE ViewPatterns #-}
 import Buster.Plugin
 import Control.Exception (bracket)
+import Data.List
 import Data.Time.Clock
 import Database.HDBC
 import Database.HDBC.Sqlite3 (connectSqlite3)
 
-main = pluginMain $ commandPlugin [("rq", rq)]
+main = pluginMain $ commandPlugin [("rq", rq), ("!rq", rq3)]
+
+rq ch ps = do r <- io (withDB $ randomQuery 1 (words ps))
+              respondChat ch (showQuotes r)
+rq3 ch ps = do r <- io (withDB $ randomQuery 3 (words ps))
+               respondChat ch (showQuotes r)
 
 withDB = bracket (connectSqlite3 "data/quotegrabs.sqlite") disconnect
 
-rq ch nm = showQuote =<< io (withDB randomQuery)
+randomQuery :: (IConnection conn) => Int -> [String] -> conn -> IO [[SqlValue]]
+randomQuery num nicks conn = quickQuery' conn (q1 ++ q2 ++ q3) ps
   where
-    randomQuery conn = quickQuery' conn (q1 ++ q2 ++ q3) ps
-    (q1, q3) = ("SELECT quote FROM quotegrabs ", "ORDER BY RANDOM() LIMIT 1")
-    (q2, ps) = case words nm of []    -> ("", [])
-                                (n:_) -> ("WHERE nick = ? ", [toSql n])
-    showQuote [[q]] = respondChat ch $ maybe "Bad quote." id (fromSql q)
-    showQuote _     = respondChat ch $ "No quotes found."
+    q1 = "SELECT quote FROM quotegrabs "
+    q3 = "ORDER BY RANDOM() LIMIT ?"
+    (q2, ps) = case nicks of []  -> ("", [toSql num])
+                             n:_ -> ("WHERE nick = ? ", [toSql n, toSql num])
+
+showQuotes [] = "No quotes found."
+showQuotes qs = intercalate " " $ map extract qs
+  where
+    extract [fromSql -> Just s] = s
+    extract _                   = "[invalid quote]"
 
 -- vi: set sw=4 ts=4 sts=4 tw=79 ai et nocindent:
